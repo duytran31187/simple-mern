02c9e9a3c929692b86cb0fcce3f923039f2adcc7
Duy Tran <duytran31187@gmail.com>
2024-05-11 16:02:23 +0700

    minor change

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 const apiAuth = require('../middlewares/apiAuth');
 
 
-router.use('/', cors({origin: 'http://localhost:3000'})) // apply cors for GET only
-router.use('/', (req, res, next) => {
+router.use('/', cors({origin: 'http://localhost:3000'}))
+router.use((req, res, next) => {
     console.log(`apply this middleware for all methods`, req.method);
      next(); 
  });
-router.get('/', apiAuth); // apply auth middleware to GET only
+router.get(apiAuth); // apply auth middleware to GET only
 
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
